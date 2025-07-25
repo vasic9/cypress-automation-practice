a859383e670a869b1ee674b2002380125422af2a
nemanja <57727863+vasic9@users.noreply.github.com>
2025-07-25 11:29:10 +0200

    added negative test case, testing only it

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -2,11 +2,18 @@
 
 describe('Test for Contact Us form', () => {
     it('should submit the contact form successfully', () => {
-       cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.get('[name="first_name"]').type('Geralt');
        cy.get('[name="last_name"]').type('of Rivia');
-       cy.get('[name="email"]').type('geralt@something.com')
-       cy.get('[name="message"]').type("Lambert, Lambert, what a ...jolly nice chap!")
+       cy.get('[name="email"]').type('geralt@something.com');
+       cy.get('[name="message"]').type("Lambert, Lambert, what a ...jolly nice chap!");
+       cy.get('[type="submit"]').click();
     })
-
+    it.only('should not be able to submit the contact form successfully', () => {
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.get('[name="first_name"]').type('Geralt');
+       cy.get('[name="last_name"]').type('of Rivia');
+       cy.get('[name="message"]').type("Lambert, Lambert, what a ...jolly nice chap!");
+       cy.get('[type="submit"]').click();
+    })
 })
