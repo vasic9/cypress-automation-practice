448b7b9b8fbb0829d26cdc034f31aec87b2b439b
nemanja <57727863+vasic9@users.noreply.github.com>
2025-07-28 22:59:15 +0200

    added x-path for contact us and submit buttons

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,12 +1,14 @@
 /// <reference types = "Cypress" />
+/// <reference types = "cypress-xpath" />
 
 describe('Test for Contact Us form', () => {
     it('should submit the contact form successfully', () => {
         cy.visit('https://automationteststore.com/')
-       cy.get('.info_links_footer > :nth-child(5) > a').click();
+       cy.get("a[href$='contact']").click();
+       //cy.xpath("//a[contains(@href, 'contact')]").click();
        cy.get('#ContactUsFrm_first_name').type('Geralt');
        cy.get('#ContactUsFrm_email').type('geralt@something.com');
        cy.get('#ContactUsFrm_enquiry').type("Hmmm...");
-       cy.get('.col-md-6 > .btn').click();
+       cy.get("button[title='Submit']").click();
     })
 })
