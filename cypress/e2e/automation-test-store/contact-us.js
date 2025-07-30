1fe3455d7d64f16bcf24946f88cbb532333635f2
nemanja <57727863+vasic9@users.noreply.github.com>
2025-07-30 21:47:54 +0200

    Added assertions for checking message after successfully submiting

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -8,7 +8,9 @@ describe('Test for Contact Us form', () => {
        //cy.xpath("//a[contains(@href, 'contact')]").click();
        cy.get('#ContactUsFrm_first_name').type('Geralt');
        cy.get('#ContactUsFrm_email').type('geralt@something.com');
+       cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
        cy.get('#ContactUsFrm_enquiry').type("Hmmm...");
        cy.get("button[title='Submit']").click();
+       cy.get('.mb40 > p').should('have.text', 'Your enquiry has been successfully sent to the store owner!');
     })
 })
--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,15 +1,16 @@
 /// <reference types = "Cypress" />
 
 describe('Test for Contact Us form', () => {
-    it('should submit the contact form successfully', () => {
+    it.only('should submit the contact form successfully', () => {
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.get('[name="first_name"]').type('Geralt');
        cy.get('[name="last_name"]').type('of Rivia');
        cy.get('[name="email"]').type('geralt@something.com');
        cy.get('[name="message"]').type("Lambert, Lambert, what a ...jolly nice chap!");
        cy.get('[type="submit"]').click();
+       cy.get('h1').should('have.text', 'Thank You for your Message!');
     })
-    it.only('should not be able to submit the contact form successfully', () => {
+    it('should not be able to submit the contact form successfully', () => {
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.get('[name="first_name"]').type('Geralt');
        cy.get('[name="last_name"]').type('of Rivia');
