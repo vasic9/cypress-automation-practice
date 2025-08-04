5342a1ddeeb05ff99ef68f1b0cfebe9949bc5037
nemanja <57727863+vasic9@users.noreply.github.com>
2025-08-04 16:44:22 +0200

    added contains assertion for false test

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,7 +1,7 @@
 /// <reference types = "Cypress" />
 
 describe('Test for Contact Us form', () => {
-    it.only('should submit the contact form successfully', () => {
+    it('should submit the contact form successfully', () => {
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.get('[name="first_name"]').type('Geralt');
        cy.get('[name="last_name"]').type('of Rivia');
@@ -10,11 +10,12 @@ describe('Test for Contact Us form', () => {
        cy.get('[type="submit"]').click();
        cy.get('h1').should('have.text', 'Thank You for your Message!');
     })
-    it('should not be able to submit the contact form successfully', () => {
+    it.only('should not be able to submit the contact form successfully', () => {
         cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
        cy.get('[name="first_name"]').type('Geralt');
        cy.get('[name="last_name"]').type('of Rivia');
        cy.get('[name="message"]').type("Lambert, Lambert, what a ...jolly nice chap!");
        cy.get('[type="submit"]').click();
+       cy.get('body').contains('Error: all fields are required');
     })
 })
