6be2dfcdd6e608b332143503136da2fb46132132
nemanja <57727863+vasic9@users.noreply.github.com>
2025-09-01 18:10:34 +0200

    Added data-tables, handling and asserting data

---
+++ cypress/e2e/webdriver-uni/data-tables.js
@@ -0,0 +1,36 @@
+/// <reference types = "cypress" />
+
+describe("Handling data", () => {
+  beforeEach(() => {
+    cy.visit("http://webdriveruniversity.com/");
+    cy.get("#data-table").invoke("removeAttr", "target").click();
+  })
+  it("Calculate total age of users, plus assertion", () => {
+    let userDetails = [];
+    let userAge = 0;
+    cy.get('#thumbnail-1 td').each(($el, index, $list) => { //selecting the table
+        userDetails[index] = $el.text(); //extracting text into array
+    }).then(() => {
+        for(let i = 0; i < userDetails.length; i++){ //looping through the array and extracting numbers (age)
+            if(Number(userDetails[i])) {
+                userAge += Number(userDetails[i]);
+            }
+            cy.log(userDetails[i]);
+        }
+        cy.log("Total age: " + userAge);
+        expect(userAge).to.eq(322);
+    })
+  });
+  it.only("Assert the age of the user based on the name", () => {
+    cy.get("#thumbnail-1 tr td:nth-child(2)").each(($el, index, $list) => { //n-th(2) child of td are last names
+        const text = $el.text(); //extracting text from last names
+        if (text.includes('Woods')) { 
+            cy.get("#thumbnail-1 tr td:nth-child(2)").eq(index).next() //using next for age column
+            .then(function(age) {
+                const userAge = age.text(); //storing age text in const
+                expect(userAge).to.equal("80");
+            })
+        }
+    })
+  })
+})
